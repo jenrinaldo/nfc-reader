a44d40f9b8295918aee89ba5c70b8c2bd1a1b369
errics7 <ebernedy@gmail.com>
2019-06-14 15:14:12 +0700

    Create block1_read.py
    ini kodingan biar bisa fokusin disalah satu block aja ..

---
+++ block1_read.py
@@ -0,0 +1,22 @@
+from smartcard.System import readers
+from smartcard.util import toHexString
+from smartcard.ATR import ATR
+from smartcard.CardType import AnyCardType
+import sys
+
+
+r = readers()
+reader = r[0]
+connection = reader.createConnection()
+connection.connect()
+
+
+COMMAND = [0xFF, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00, 0x01, 0x60, 0x00] ## Otentikasi
+																	   ## [ FF, 86, 00, 00, 05, 01, 00, Block_Number, Type_Key, 00]
+																	   ## Type_Key = 60->A, 61->B
+data, sw1, sw2 = connection.transmit(COMMAND)
+
+COMMAND = [0xFF, 0xB0, 0x00, 0x02, 0x10]## Read [ FF, B0, 00, Block_Number, Read_Byte ]
+										## Read Byte = 16byte-> 10, 4byte->4=04
+data, sw1, sw2 = connection.transmit(COMMAND)
+print (''.join(chr(i) for i in data))
