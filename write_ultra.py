7277ec93a75e67435639d899cb3f3119522c9869
Fajri Rahman <fajri.achmad55@gmail.com>
2019-06-17 15:57:33 +0700

    Update write_ultra.py

--- write_ultra.py
+++ write_ultra.py
@@ -2,43 +2,31 @@ from smartcard.System import readers
 from smartcard.util import toHexString
 from smartcard.ATR import ATR
 from smartcard.CardType import AnyCardType
+from itertools import islice 
 import sys
-
 
 r = readers()
 reader = r[0]
 connection = reader.createConnection()
 connection.connect()
 
-
-# COMMAND = [0xFF, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00, 0x01, 0x60, 0x00]
-# data, sw1, sw2 = connection.transmit(COMMAND)
-# if (sw1, sw2) == (0x90, 0x0):
-    # print ("Status: Decryption all sector using key #0 as Key A successful.")
-masukan = input("Input NIM : ")
+masukan = list(input("Input NIM : "))
 x = len(masukan)
 bfr = [" "]
-COMMAND = [0xFF, 0xD6, 0x00, 0x04, 0x04]
 
-if x <= 4:
-    COMMAND.extend([ord(c) for c in masukan])
-    for y in range (4-x):
-        COMMAND.extend([ord(c) for c in bfr])
-    data, sw1, sw2 = connection.transmit(COMMAND)
-    print ("Status words: %02X %02X" % (sw1, sw2))
-    if (sw1, sw2) == (0x90, 0x0):
-        print ("Status: Key is loaded successfully to key #0.")
-    elif (sw1, sw2) == (0x63, 0x0):
-        print ("Status: Failed to load key.")
-        
-elif x == 4:
-    COMMAND.extend([ord(c) for c in masukan])
-    data, sw1, sw2 = connection.transmit(COMMAND)
-    print ("Status words: %02X %02X" % (sw1, sw2))
-    if (sw1, sw2) == (0x90, 0x0):
-        print ("Status: Key is loaded successfully to key #0.")
-    elif (sw1, sw2) == (0x63, 0x0):
-        print ("Status: Failed to load key.")
-    
+if x <= 16:
+    for y in range (16-x):
+        masukan.extend(bfr)
+    splitted = [masukan[i * 4:(i + 1) * 4] for i in range((len(masukan) + 4 - 1) // 4 )]
 else :
-	print ("Input Terlalu Banyak!")
+    print ("Input Terlalu Banyak!")
+    
+for page in range(4, 8):
+    COMMAND = [0xFF, 0xD6, 0x00, page, 0x04]
+    COMMAND.extend([ord(c) for c in splitted[page - 4]])
+    data, sw1, sw2 = connection.transmit(COMMAND)
+print ("Status words: %02X %02X" % (sw1, sw2))
+if (sw1, sw2) == (0x90, 0x0):
+    print ("Status: Key is loaded successfully to key #0.")
+elif (sw1, sw2) == (0x63, 0x0):
+    print ("Status: Failed to load key.")    
