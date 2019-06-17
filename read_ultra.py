339f4baf3e9abc03b44042a9e2541c75ab1b770c
errics7 <ebernedy@gmail.com>
2019-06-17 14:32:21 +0700

    Update read_ultra.py

--- read_ultra.py
+++ read_ultra.py
@@ -3,7 +3,7 @@ from smartcard.util import toHexString
 from smartcard.ATR import ATR
 from smartcard.CardType import AnyCardType
 import sys
-import clipboard
+#import clipboard
 
 # get the reader
 r = readers()
@@ -16,13 +16,16 @@ reader = r[0]
 # connect with the card
 connection = reader.createConnection()
 connection.connect()
-# authenticate and read sector 0
-# COMMAND = [0xFF, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00, 0x04, 0x60, 0x00]
-# data, sw1, sw2 = connection.transmit(COMMAND)
+
+bfr = []
 # read block 0
-COMMAND = [0xFF, 0xB0, 0x00, 0x04, 0x04]
-data, sw1, sw2 = connection.transmit(COMMAND)
-print ("\nNIM : "+ ''.join(chr(i) for i in data))
+for page in range (4, 8):
+	COMMAND = [0xFF, 0xB0, 0x00, page, 0x04]
+	data, sw1, sw2 = connection.transmit(COMMAND)
+	bfr.extend(data)
+
+print ("\nNIM : "+ ''.join(chr(i) for i in bfr))
 NIM = ''.join(chr(i) for i in data)
-clipboard.copy(NIM)
-text = clipboard.paste()
+
+#clipboard.copy(NIM)
+#text = clipboard.paste()
