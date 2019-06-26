ba9cda87aeeb7ebd357dda2438162d650c1498f2
errics7 <ebernedy@gmail.com>
2019-06-26 08:41:57 +0700

    Update Gabung.ino

--- Gabung/Gabung.ino
+++ Gabung/Gabung.ino
@@ -1,153 +1,141 @@
-#include <SPI.h>
 #include <MFRC522.h>
+#include <SPI.h>
 
-#define RST_PIN         D1           // Configurable, see typical pin layout above
-#define SS_PIN          D2          // Configurable, see typical pin layout above
+#define RST_PIN D1
+#define SS_PIN  D2
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
-String command;
+// Create MFRC522 instance
+MFRC522 mfrc522(SS_PIN, RST_PIN);  
 
+// Some variabels we need
+byte block, len, buffer[18];
 
-void baca(){
-      // -------------- KEY : FFFFFFFFFFFFh ----------------------------
-      MFRC522::MIFARE_Key key;
-      for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
- 
-      //some variables we need
-      byte block;
-      byte len;
+// MFR buffer for status code
+MFRC522::StatusCode status;
 
-      MFRC522::StatusCode status;
-    
-      //-------------------------------------------
-     
-          byte buffer1[18];
-          block = 1;
-          len = 18;
-        
-          // ------------------ OTENTIKASI --------------------------------
-          status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
-            if (status != MFRC522::STATUS_OK) {
-            Serial.print("Authentication failed: ");
-            Serial.println(mfrc522.GetStatusCodeName(status));
-            return;
-          }
-        
-          status = mfrc522.MIFARE_Read(block, buffer1, &len);
-          if (status != MFRC522::STATUS_OK) {
-            Serial.print("Reading failed: ");
-            Serial.println(mfrc522.GetStatusCodeName(status));
-            return;
-          }
-          // --------------------------------------------------------------
-          
-          // ---------------PRINT NIM------------------------
-
-          for (uint8_t i = 0; i < 16; i++)
-          {
-            if (buffer1[i] != 32)
-            {
-              Serial.write(buffer1[i]);
-            }
-          }
-          Serial.print(" ");
-        
-        
-          //------------------------------------------------
-        
-       
-          delay(1000); //change value if you want to read cards faster
-        
-            mfrc522.PICC_HaltA();
-            mfrc522.PCD_StopCrypto1();
-                              
-}
-
-void tulis()
-{
-    // ---------------------- KEY : FFFFFFFFFFFFh at chip delivery from the factory.
-    MFRC522::MIFARE_Key key;
-    for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
-
-    Serial.println(F("\nTulis NIM anda.. "));
-    
-    Serial.print(F("Card UID: "));    //Dump UID
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-      Serial.print(mfrc522.uid.uidByte[i], HEX);
-    }
-   
-    byte buffer[20];
-    byte block;
-    MFRC522::StatusCode status;
-    byte len;
-  
-    Serial.setTimeout(20000L);
-    
-    len = Serial.readBytesUntil('\n', (char *) buffer, 20) ; // read family name from serial
-  
-    if (len <= 16)
-    {
-      for (byte i = len; i < 16; i++) buffer[i] = ' ';     // pad with spaces
-    
-      block = 1;
-      //Serial.println(F("Authenticating using key A..."));
-      status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
-      if (status != MFRC522::STATUS_OK) {
-        Serial.print(F("PCD_Authenticate() failed: "));
-        Serial.println(mfrc522.GetStatusCodeName(status));
-        return;
-      }
-      else Serial.println(F("PCD_Authenticate() success: "));
-      ///////////////////////////////////////////////////////////////////////////////////////////////
-      
-      // Write block
-      status = mfrc522.MIFARE_Write(block, buffer, 16);
-      if (status != MFRC522::STATUS_OK) {
-        Serial.print(F("MIFARE_Write() failed: "));
-        Serial.println(mfrc522.GetStatusCodeName(status));
-        return;
-      }
-      else Serial.println(F("MIFARE_Write() success: "));
-      ///////////////////////////////////////////////////////////////////////////////////////////////
-    
-      Serial.println(" ");
-      mfrc522.PICC_HaltA(); // Halt PICC
-      mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD 
-    }
-    else 
-    {
-      Serial.println("Masukan Terlalu Banyak, Maksimal 16 karakter saja :)");
-    }
-}
-
-
+// Buffer for reader mode
+String reader_mode;
 
 void setup() {
-  Serial.begin(9600);                                           // Initialize serial communications with the PC
-  SPI.begin();                                                  // Init SPI bus
-  mfrc522.PCD_Init();                                           // Init MFRC522 card
+    Serial.begin(9600);   // Initialize serial communications with the PC
+    SPI.begin();          // Init SPI bus
+    mfrc522.PCD_Init();   // Init MFRC522 card
 }
 
-void loop(){
- // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
- if ( ! mfrc522.PICC_IsNewCardPresent()) {
-  return;
- }
-          
- // Select one of the cards
- if ( ! mfrc522.PICC_ReadCardSerial()) {
-  return;
- }
- 
- if (Serial.available())
- {
-   command = Serial.readStringUntil('\n');
-   if (command.equals("tulis")){
-    tulis();
-   }
- }
- 
- baca();
+void loop() {
+    // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle
+    if ( ! mfrc522.PICC_IsNewCardPresent()) {
+        return;
+    }
+    
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial()) {
+        return;
+    }
+  
+    if (Serial.available()) {
+        reader_mode = Serial.readStringUntil('#');
+        if (reader_mode.equals("write") == true) {
+            write_card();
+        }
+    }
+    else {
+        read_card();
+    }
+}
 
+void authenticate_key() {
+    MFRC522::MIFARE_Key key;
+    for (byte i = 0; i < 6; i++) {
+        key.keyByte[i] = 0xFF;
+    }
+
+    Serial.print("Authenticating...\n");
+    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+}
+
+void read_card() {
+    // Set block 1 as default block to read
+    block = 1;
+    len = 18;
+    
+    // Authentication
+    authenticate_key();
+    
+    // Authentication status
+    if (status != MFRC522::STATUS_OK) {
+        Serial.println("Authentication failed!");
+
+        // Delay for changing card(s)
+        delay(5000);
+        // Stop reading
+        mfrc522.PICC_HaltA();
+        mfrc522.PCD_StopCrypto1();
+        return;
+    }
+        
+    // Read the block 1
+    status = mfrc522.MIFARE_Read(block, buffer, &len);
+    if (status != MFRC522::STATUS_OK) {
+        Serial.println("Reading failed!");
+        return;
+    }
+    
+    // Print what's inside the block 1
+    for (uint8_t i = 0; i < 16; i++) {
+        if (buffer[i] != 32)
+            Serial.write(buffer[i]);
+    }
+     exit_card();
+}
+
+void write_card() {
+    Serial.setTimeout(20000L);
+
+    len = Serial.readBytesUntil('*', (char *) buffer, 18);
+    if (len <= 16) {
+        for (byte i = len; i < 16; i++)
+            buffer[i] = ' ';
+
+        // Select default block to be written
+        block = 1;
+        
+        // Authentication
+        authenticate_key();
+
+        // Authentication status
+        if (status != MFRC522::STATUS_OK) {
+            Serial.println("Authentication failed!");
+    
+            // Delay for changing card(s)
+            delay(5000);
+            
+            // Stop reading
+            mfrc522.PICC_HaltA();
+            mfrc522.PCD_StopCrypto1();
+            return;
+        }
+
+        // Write block
+        status = mfrc522.MIFARE_Write(block, buffer, 16);
+        if (status != MFRC522::STATUS_OK) {
+            Serial.print("Write failed!");
+            return;
+        }
+        else {
+            Serial.print("Write success!");
+            exit_card();
+        }            
+    }
+}
+
+void exit_card() {
+    // End
+    Serial.print(" ");
+    // Delay for changing card(s)
+    delay(1000);
+        
+    // Stop reading the card
+    mfrc522.PICC_HaltA();
+    mfrc522.PCD_StopCrypto1();
 }
